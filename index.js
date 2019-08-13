ea83f28abb7223280e46f91b6f6159ff0cb4a175
Adel <htwr86@gmail.com>
2019-08-13 17:01:00 +0300

    copyright from txt added

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-const jokes = require('./joke.js');
+//const jokes = require('./joke.js');
+import {jokes} from './joke';
+import fs from 'fs';
 
 jokes.getOne()
   .then(joke => {
     document.getElementById('joke').innerHTML = joke;
   });
+
+const copy = fs.readFileSync(__dirname +'/copyright.txt', 'utf8');
+document.getElementById('copy').innerHTML = copy;
